b75213ded4e5d10268e0178aba5a2fb695d80a28
Gautham R K <120078927+Gautham05@users.noreply.github.com>
2023-09-25 14:48:02 +0530

    Add files via upload

---
+++ Day 8/src/components/Signup.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import "..//css/login.css";
+
+function Signup() {
+
+
+  return (
+    <div id="body">
+    <div className="signup-form">
+      <div className="container">
+        <div className="header">
+          <h1>Create an Account</h1>
+        </div>
+        <form>
+          <div className="input">
+            <input type="text"   placeholder="Name" />
+          </div>
+          <div className="input">
+            <input type="email"   placeholder="Email" />
+          </div>
+          <div className="input">
+            <input type="password"  placeholder="Password" />
+          </div>
+          <div className="input">
+            <input  type="password"   placeholder="Confirm Password" />
+          </div>
+          <Link to="/">
+          <input className="signup-btn" type="submit" value="SIGN UP" />
+          </Link>
+        </form>
+        <Link to="/">
+          <p>Already have an account? Sign in</p>
+        </Link>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default Signup;
---
+++ Day 8/src/components/login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import "..//css/login.css";
+import { useDispatch } from 'react-redux';
+import { login } from './redux/authSlice';
+
+
+function Login() {
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: '',
+});
+
+const navigate = useNavigate();
+const dispatch = useDispatch();
+
+const handleLoginChange = (e) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+};
+
+const handleLoginSubmit = (e) => {
+  e.preventDefault();
+  dispatch(login(loginData.username));
+  console.log(loginData);
+  navigate('/dashboard');
+};
+
+
+  return (
+    <div id="body">
+      <div className="signup-form">
+        <div className="container">
+          <div className="header">
+            <h1>LOGIN</h1>
+            <p>Enter Credentials For Login</p>
+          </div>
+          <form onSubmit={handleLoginSubmit}>
+            <div className="input">
+              <input required type="text"  placeholder="Username" name='username' onChange={handleLoginChange}/>
+            </div>
+            <div className="input">
+              <input required type="password"  placeholder="Password" name='password' onChange={handleLoginChange}/>
+            </div>
+            <input  className="signup-btn" type="submit" value="LOGIN" />
+          </form>
+          <Link to="/register"><p>New User? Signup Now to create an account!</p></Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
